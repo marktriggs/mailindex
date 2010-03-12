0c7ad6be1561a51eec39fc05f50969708d7b943e
Mark Triggs <mark@dishevelled.net>
2010-03-12 11:40:37 +1100

    Updated to Lucene 2.9

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject mailindex "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-		 [org.apache.lucene/lucene-core "2.4.1"]
-		 [org.apache.lucene/lucene-highlighter "2.4.1"]]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+		 [org.apache.lucene/lucene-core "2.9.2"]
+		 [org.apache.lucene/lucene-highlighter "2.9.2"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]] 
+  :namespaces :all
   :main MailIndex)
