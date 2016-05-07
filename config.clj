66f67c95b2db5ff0cb6b5afb9f650b0dba44faac
Mark Triggs <mark@dishevelled.net>
2016-05-07 15:51:58 +1000

    Increase max results and RAM.  Welcome to 2016!

--- config.clj
+++ config.clj
@@ -1,7 +1,7 @@
 {:indexfile "/home/mst/.mail-index"
  :port 4321
  :reindex-frequency 60000
- :max-results 1000
+ :max-results 10000
  :optimize-hour 4                       ; optimise at 4am
  :backends [{:name "gnus-mail"
              :backend net.dishevelled.mailindex.backends.nnml
