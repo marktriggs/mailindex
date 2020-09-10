6c062159617c2f5a0b72dd9e7952d33daf28439f
Mark Triggs <mark@dishevelled.net>
2020-09-10 16:50:38 +1000

    More results for 2020!

--- config.clj
+++ config.clj
@@ -1,7 +1,7 @@
 {:indexfile "/mnt/ssd/mst-home/mail-index"
  :port 4321
  :reindex-frequency 60000
- :max-results 10000
+ :max-results 100000
  :optimize-hour 4                       ; optimise at 4am
  :backends [{:name "gnus-mail"
              :backend net.dishevelled.mailindex.backends.nnml
